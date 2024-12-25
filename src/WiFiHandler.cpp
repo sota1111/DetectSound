8525436d17f5208dc7f65f0781a24d0ec7d20676
sota1111 <sota.moro@gmail.com>
2024-12-26 07:32:26 +0900

    wifi接続タイムアウト処理追加

--- src/WiFiHandler.cpp
+++ src/WiFiHandler.cpp
@@ -3,7 +3,15 @@
 void WiFiHandler::connectWiFi(const char* ssid, const char* password) {
     M5.Lcd.printf("Connecting to %s", ssid);
     WiFi.begin(ssid, password);
+
+    unsigned long startAttemptTime = millis();
+    const unsigned long timeout = 30000; // 30秒タイムアウト
+
     while (WiFi.status() != WL_CONNECTED) {
+        if (millis() - startAttemptTime >= timeout) {
+            M5.Lcd.println("\nWiFi Connection Timeout!");
+            return;
+        }
         delay(500);
         M5.Lcd.print('.');
     }
