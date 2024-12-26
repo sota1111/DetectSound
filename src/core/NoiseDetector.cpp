a03d51c9eea6b2c79c946a1448a6705b9d5abe6f
sota1111 <sota.moro@gmail.com>
2024-12-27 08:25:12 +0900

    ファイル名を日付時間に変更

--- src/core/NoiseDetector.cpp
+++ src/core/NoiseDetector.cpp
@@ -14,19 +14,27 @@ void NoiseDetector::logNoiseTimestamp() {
     struct tm timeInfo;
     if (getLocalTime(&timeInfo)) {
         char timestamp[64];
-        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &timeInfo);
-        sdcardHandler.writeSDCard("/Data/noise.txt", timestamp);
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d_%H-%M-%S", &timeInfo);
+
+        // Generate file name based on the current timestamp
+        char fileName[128];
+        snprintf(fileName, sizeof(fileName), "/Data/noise_%s.txt", timestamp);
+
+        // Write the timestamp into the newly created file
+        sdcardHandler.writeSDCard(fileName, timestamp);
         M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
         M5.Lcd.setCursor(0,80);
         M5.Lcd.println("Success to write time on SD card");
         delay(1000);
     } else {
+        // Display error message if the current time cannot be obtained
         M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
-        M5.Lcd.setCursor(0,80);
+        M5.Lcd.setCursor(0, 80);
         M5.Lcd.println("Current time has not been obtained.");
         delay(1000);
     }
 }
+
 
 void NoiseDetector::detectNoise(int micValue) {
     maxMicValue = micValue;
