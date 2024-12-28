83df6859cd9a6fa2ecd907d8af12ab81db083972
sota1111 <sota.moro@gmail.com>
2024-12-28 10:11:02 +0900

    buf処理を追加

--- src/core/NoiseDetector.h
+++ src/core/NoiseDetector.h
@@ -6,13 +6,19 @@
 #include <time.h>
 #include "../config/config.h"
 
+#define RECORD_MAX_LEN 1/TIME_IRQ*1000*1000
+
 class NoiseDetector {
 private:
+    int16_t val_buf[RECORD_MAX_LEN];
+    int write_index;
     int maxMicValue;
     void logNoiseTimestamp();
 
 public:
+    NoiseDetector();
     void initNoiseDetector();
+    void updateBuffer(int micValue);
     void detectNoise(int micValue);
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,16 @@ WaveformDrawer waveformDrawer;
 
 volatile int micValue = 0;
 volatile int maxMicValue = 0;
+volatile unsigned long irqTime;
 hw_timer_t *timer = NULL;
 
 void IRAM_ATTR onTimer() {
+    unsigned long startTime = micros();
     micValue = analogRead(36);
     waveformDrawer.updateBuffer(micValue);
+    noiseDetector.updateBuffer(micValue);
     maxMicValue = waveformDrawer.calcMaxADValue(micValue);
+    irqTime = micros() - startTime;
 }
 
 void setup() {
@@ -26,15 +30,20 @@ void setup() {
     noiseDetector.initNoiseDetector();
     timer = timerBegin(0, 80, true);         // タイマー0を設定 (80分周 -> 1µs)
     timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, 1000, true);      // 1msごとに割り込み
+    timerAlarmWrite(timer, TIME_IRQ, true);
     timerAlarmEnable(timer);
 }
 
 void loop() {
     M5.Lcd.fillScreen(TFT_BLACK);
-    waveformDrawer.drawMaxADValue(micValue);
+    waveformDrawer.drawMaxADValue(maxMicValue);
     noiseDetector.detectNoise(maxMicValue);
     waveformDrawer.drawWaveform();
+    // 画面左下にIRQの時間を表示
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setCursor(0, 220);
+    M5.Lcd.printf("IRQ Time: %lu us", irqTime);
     delay(20);
 }
 
