e172e92f375a96b86a2b70c85ad9936c6e0817d0
sota1111 <sota.moro@gmail.com>
2024-12-31 17:46:52 +0900

    LCD表示を止め、実行時間を計測

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 WiFiHandler wifiHandler;
 WaveformDrawer waveformDrawer;
 
-volatile int maxMicValue = 0;
-
 void setup() {
     M5.begin();
     wifiHandler.connectWiFi(WIFI_SSID, WIFI_PASSWORD);
@@ -20,16 +18,7 @@ void setup() {
 }
 
 void loop() {
-    //M5.Lcd.fillScreen(TFT_BLACK);
-    //waveformDrawer.drawMaxADValue(maxMicValue);
     noiseDetector.storeNoise();
-    //waveformDrawer.drawWaveform();
-    // 画面左下にIRQの時間を表示
-    //M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-    //M5.Lcd.setTextSize(1);
-    //M5.Lcd.setCursor(0, 220);
-    //M5.Lcd.printf("IRQ Time: %lu us", irqTime);
-    //delay(20);
 }
 
 
