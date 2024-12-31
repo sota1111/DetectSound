0423ff7604f5f24a1cddad3dc1a42e5b024c5075
sota1111 <sota.moro@gmail.com>
2024-12-31 12:42:57 +0900

    割り込みが停止しないので、処理を移す必要あり

--- src/main.cpp
+++ src/main.cpp
@@ -15,12 +15,29 @@ volatile unsigned long irqTime;
 hw_timer_t *timer = NULL;
 
 void IRAM_ATTR onTimer() {
-    unsigned long startTime = micros();
+    //unsigned long startTime = micros();
     micValue = analogRead(36);
-    waveformDrawer.updateBuffer(micValue);
+    //waveformDrawer.updateBuffer(micValue);
     noiseDetector.updateBuffer(micValue);
-    maxMicValue = waveformDrawer.calcMaxADValue(micValue);
-    irqTime = micros() - startTime;
+    //maxMicValue = waveformDrawer.calcMaxADValue(micValue);
+    //irqTime = micros() - startTime;
+}
+
+void startTimer() {
+    timer = timerBegin(0, 80, true);         // タイマー0を設定 (80分周 -> 1µs)
+    timerAttachInterrupt(timer, &onTimer, true);
+    timerAlarmWrite(timer, TIME_IRQ, true);
+    timerAlarmEnable(timer);
+}
+
+void restartTimer() {
+    M5.Lcd.setCursor(0, 100);
+    M5.Lcd.println("Restart Timer");
+    delay(1000);
+    M5.Lcd.fillScreen(TFT_BLACK);
+    delay(1000);
+    timerRestart(timer);
+    timerStart(timer);
 }
 
 void setup() {
@@ -28,23 +45,24 @@ void setup() {
     wifiHandler.connectWiFi(WIFI_SSID, WIFI_PASSWORD);
     wifiHandler.synchronizeTime();
     noiseDetector.initNoiseDetector();
-    timer = timerBegin(0, 80, true);         // タイマー0を設定 (80分周 -> 1µs)
-    timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, TIME_IRQ, true);
-    timerAlarmEnable(timer);
+    M5.Lcd.fillScreen(TFT_BLACK);
+    startTimer();
 }
 
 void loop() {
-    M5.Lcd.fillScreen(TFT_BLACK);
-    waveformDrawer.drawMaxADValue(maxMicValue);
-    noiseDetector.detectNoise(maxMicValue);
-    waveformDrawer.drawWaveform();
+    //M5.Lcd.fillScreen(TFT_BLACK);
+    //waveformDrawer.drawMaxADValue(maxMicValue);
+    noiseDetector.storeNoise(timer);
+    if(noiseDetector.judgeRestartTimer()){
+        restartTimer();
+    }
+    //waveformDrawer.drawWaveform();
     // 画面左下にIRQの時間を表示
-    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-    M5.Lcd.setTextSize(1);
-    M5.Lcd.setCursor(0, 220);
-    M5.Lcd.printf("IRQ Time: %lu us", irqTime);
-    delay(20);
+    //M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    //M5.Lcd.setTextSize(1);
+    //M5.Lcd.setCursor(0, 220);
+    //M5.Lcd.printf("IRQ Time: %lu us", irqTime);
+    //delay(20);
 }
 
 
