e5f036177d7029423e67886b5f1722ce686ef375
sota1111 <sota.moro@gmail.com>
2024-12-31 12:42:57 +0900

    割り込みが停止しないので、処理を移す必要あり

--- src/core/NoiseDetector.cpp
+++ src/core/NoiseDetector.cpp
@@ -6,7 +6,7 @@ SDCardHandler sdcardHandler;
 SpeakerHandler speakerHandler;
 
 // 初期化
-NoiseDetector::NoiseDetector() : write_index(0){
+NoiseDetector::NoiseDetector() : isRequestSpeaker(false), isDataStored(false), isTimerStopped(false), write_index(0), detect_index(0) {
     memset(val_buf, 0, sizeof(val_buf));
 }
 
@@ -15,60 +15,95 @@ void NoiseDetector::initNoiseDetector() {
 }
 
 void NoiseDetector::updateBuffer(int micValue) {
-    val_buf[write_index] = micValue;
-    write_index = (write_index + 1) % RECORD_MAX_LEN;
+    static unsigned int detect_count = 0;
+    static bool isNoiseDetected = false;
+
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.printf("micValue: %d", micValue);
+
+    if(!isDataStored){
+        // ノイズを検出前後のデータを記録        
+        write_index = (write_index + 1) % RECORD_MAX_LEN;
+        val_buf[write_index] = micValue;
+        // M5.Lcd.setCursor(0, 200);
+        // M5.Lcd.printf("write_index: %d", write_index);
+        if((micValue > NOISE_CONSTANT_VALUE) && (isNoiseDetected == false)){
+            isNoiseDetected = true;
+            isRequestSpeaker = true;
+            detect_index = write_index;
+            M5.Lcd.setCursor(0, 20);
+            M5.Lcd.printf("NOISE DETECTED");
+        }
+        if(isNoiseDetected){
+            detect_count++;
+        }
+        // ノイズ検出後、データを貯め続ける。
+        if(detect_count > RECORD_AFTER_LEN){
+            isDataStored = true;
+            isNoiseDetected = false;
+            detect_count = 0;
+            M5.Lcd.setCursor(0, 40);
+            M5.Lcd.printf("BUFFER FULL");
+        }
+    }
 }
 
 void NoiseDetector::logNoiseTimestamp() {
     struct tm timeInfo;
+    String csvData = "";
+    // detect_index+RECORD_AFTER_LENがcsvDataの先頭になるようにする
+    int startIndex = detect_index + RECORD_AFTER_LEN + 1;
+    int endIndex = startIndex + RECORD_MAX_LEN;
+    detect_index = 0;
+
+    for (int i = startIndex; i < endIndex; i++) {
+        csvData += String(val_buf[i % RECORD_MAX_LEN]) + ",";
+    }
+
     if (getLocalTime(&timeInfo)) {
         char timestamp[64];
         strftime(timestamp, sizeof(timestamp), "%Y-%m-%d_%H-%M-%S", &timeInfo);
 
         // CSVファイル名を生成
         char fileName[128];
-        snprintf(fileName, sizeof(fileName), "/Data/noise_%s.csv", timestamp);
-
-        // CSV形式で val_buf のデータを文字列に変換
-        String csvData = "";
-        for (int i = 0; i < RECORD_MAX_LEN; i++) {
-            csvData += String(val_buf[i]);
-            if (i < RECORD_MAX_LEN - 1) {
-                csvData += ",";
-            }
-        }
-
-        // SDカードにCSV形式でデータを書き込み
-        if (sdcardHandler.writeSDCard(fileName, csvData.c_str())) {
-            // 書き込み成功メッセージ
-            M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-            M5.Lcd.setCursor(0, 80);
-            M5.Lcd.println("Success to write data on SD card");
-        } else {
-            // 書き込み失敗メッセージ
-            M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
-            M5.Lcd.setCursor(0, 80);
-            M5.Lcd.println("Failed to write data to SD card");
-        }
-
-        delay(1000);
+        snprintf(fileName, sizeof(fileName), "/Data/log_%s.csv", timestamp);
+        File csvFile = SD.open(fileName, FILE_APPEND);
+        csvFile.print(csvData.c_str());
+        csvFile.close();
+        M5.Lcd.setCursor(0, 60);
+        M5.Lcd.printf("Noise data has been stored.");
     } else {
         // 現在時刻が取得できなかった場合のエラーメッセージ
         M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
-        M5.Lcd.setCursor(0, 80);
+        M5.Lcd.setCursor(0, 60);
         M5.Lcd.println("Current time has not been obtained.");
         delay(1000);
     }
+
 }
 
+bool NoiseDetector::judgeRestartTimer() {
+    if (isTimerStopped) {
+        isTimerStopped = false;
+        return true;
+    }
+    return false;
+}
 
-void NoiseDetector::detectNoise(int micValue) {
-    maxMicValue = micValue;
-    if (maxMicValue > NOISE_CONSTANT_VALUE) {
-        M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
-        M5.Lcd.setTextSize(FONT_SIZE);
-        M5.Lcd.drawString("NOISE", 160, 360);
+void NoiseDetector::storeNoise(hw_timer_t *timer) {
+    if (isRequestSpeaker) {
         speakerHandler.playTone(440, 100);
+        isRequestSpeaker = false;
+    }
+
+    if (isDataStored) {
+        timerStop(timer);
         logNoiseTimestamp();
+        isDataStored = false;
+        M5.Lcd.setCursor(0, 120);
+        M5.Lcd.println("NOISE STORED");
+        delay(3000);
+        M5.Lcd.fillScreen(TFT_BLACK);
+        isTimerStopped = true;
     }
 }
--- src/core/NoiseDetector.h
+++ src/core/NoiseDetector.h
@@ -6,20 +6,22 @@
 #include <time.h>
 #include "../config/config.h"
 
-#define RECORD_MAX_LEN 1/TIME_IRQ*1000*1000
-
 class NoiseDetector {
 private:
+    bool isRequestSpeaker;
+    bool isDataStored;
+    bool isTimerStopped;
+    unsigned int write_index;
+    unsigned int detect_index;
     int16_t val_buf[RECORD_MAX_LEN];
-    int write_index;
-    int maxMicValue;
     void logNoiseTimestamp();
 
 public:
     NoiseDetector();
     void initNoiseDetector();
     void updateBuffer(int micValue);
-    void detectNoise(int micValue);
+    void storeNoise(hw_timer_t *timer);
+    bool judgeRestartTimer(void);
 };
 
 #endif
